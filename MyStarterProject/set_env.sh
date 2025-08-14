fccbd40fc90e940a4b79848351c497793ffa375b
shabaz123 <shabaz123@users.noreply.github.com>
2025-08-14 15:00:07 +0100

    Add files via upload

---
+++ MyStarterProject/set_env.sh
@@ -0,0 +1,14 @@
+# File: set_env.sh
+# to run, type: source ./set_env.sh
+
+# NOTE: Use full paths, do not use '~' to indicate home folder
+
+GCC_ARMCOMPILER_MSP="/usr/share/gcc-arm-none-eabi-10.3-2021.10"
+export GCC_ARMCOMPILER_MSP
+echo "GCC_ARMCOMPILER_MSP set to: $GCC_ARMCOMPILER_MSP"
+
+MSPM0_SDK_INSTALL_DIR="/home/shabaz/development/mspm0-sdk"
+export MSPM0_SDK_INSTALL_DIR
+echo "MSPM0_SDK_INSTALL_DIR set to: $MSPM0_SDK_INSTALL_DIR"
+
+
