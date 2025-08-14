2ebd5c8f86e5a21e2ce99a3fd9d67bba5a4818b1
shabaz123 <shabaz123@users.noreply.github.com>
2025-08-14 15:01:17 +0100

    Update set_env.sh

--- MyStarterProject/set_env.sh
+++ MyStarterProject/set_env.sh
@@ -7,7 +7,7 @@ GCC_ARMCOMPILER_MSP="/usr/share/gcc-arm-none-eabi-10.3-2021.10"
 export GCC_ARMCOMPILER_MSP
 echo "GCC_ARMCOMPILER_MSP set to: $GCC_ARMCOMPILER_MSP"
 
-MSPM0_SDK_INSTALL_DIR="/home/shabaz/development/mspm0-sdk"
+MSPM0_SDK_INSTALL_DIR="/home/PLACE_USERNAME_HERE/development/mspm0-sdk"
 export MSPM0_SDK_INSTALL_DIR
 echo "MSPM0_SDK_INSTALL_DIR set to: $MSPM0_SDK_INSTALL_DIR"
 
